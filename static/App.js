6168b198d8ccb42c91b69c4b8c2dbb4c45db453d
Lucas Silva <Lucas Silva>
2016-03-31 21:47:17 -0300

    moved App.js to src and added static/App.js to gitignore

--- static/App.js
+++ static/App.js
@@ -1,4 +1,5 @@
-ReactDOM.render(
-	<h1>Hello, world!</h1>,
-	document.getElementById('example')
-);
+ReactDOM.render(React.createElement(
+	'h1',
+	null,
+	'Hello, world!'
+), document.getElementById('example'));
