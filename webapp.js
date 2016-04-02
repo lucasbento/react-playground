e7f8d7b3673eabe64bc5b2d82fa0daa5877bdec9
Lucas Bento <lucas.bsilva@outlook.com>
2016-04-02 12:25:12 -0300

    Created post endpoint

--- webapp.js
+++ webapp.js
@@ -1,25 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/api/bugs', function(req, res) {
-	var bugs = [
-		{
-			id: 1,
-			status: 'Open',
-			priority: 'P1',
-			owner: 'Joey',
-			title: 'Joey Tribbiani does not share food.'
-		},
-		{
-			id: 2,
-			status: 'Closed',
-			priority: 'P3',
-			owner: 'Chandler',
-			title: 'Ms. Chanandler Bong.'
-		}
-	];
+var bugs = [
+	{
+		id: 1,
+		status: 'Open',
+		priority: 'P1',
+		owner: 'Joey',
+		title: 'Joey Tribbiani does not share food.'
+	},
+	{
+		id: 2,
+		status: 'Closed',
+		priority: 'P3',
+		owner: 'Chandler',
+		title: 'Ms. Chanandler Bong.'
+	}
+];
 
-	res.json(bugs);
+app.use(bodyParser.json());
+
+app.get('/api/bugs', function(req, res) {
+	return res.json(bugs);
+});
+
+app.post('/api/bugs', function(req, res) {
+	var newBug = req.body.bug;
+	newBug.id = bugs.length + 1;
+	bugs.push(newBug);
+
+	return res.json(newBug);
 });
 
 // Serve static files placed into folder "static"
