69bdddce97378b3f980314c9d93ee662aa6d43a8
Lucas Bento <lucas.bsilva@outlook.com>
2016-04-02 12:16:24 -0300

    Created an endpoint for bugs

--- webapp.js
+++ webapp.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var app = express();
 
-// app.get('/', function(req, res) {
-//   res.send('Hello World!');
-// });
+app.get('/api/bugs', function(req, res) {
+	var bugs = [
+		{
+			id: 1,
+			status: 'Open',
+			priority: 'P1',
+			owner: 'Joey',
+			title: 'Joey Tribbiani does not share food.'
+		},
+		{
+			id: 2,
+			status: 'Closed',
+			priority: 'P3',
+			owner: 'Chandler',
+			title: 'Ms. Chanandler Bong.'
+		}
+	];
+
+	res.json(bugs);
+});
 
 // Serve static files placed into folder "static"
 app.use(express.static('static'));
 
 app.listen('3000', function() {
-  console.log('App listening on port 3000');
+	console.log('App listening on port 3000');
 });
