9929c3696e3273507129fc7a5eccebb97a03cf79
Lucas Bento <lucas.bsilva@outlook.com>
2016-04-02 10:45:55 -0300

    Passing data between classes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-var BugList = React.createClass({
+var BugFilter = React.createClass({
 	render: function() {
 		return (
-			<table>
+			<div className="bugFilter">
+				<span>This is where the filter will be</span>
+			</div>
+		);
+	}
+});
+
+var BugTable = React.createClass({
+	render: function() {
+		return (
+			<table border="1">
 				<thead>
 					<th>ID</th>
+					<th>Status</th>
 					<th>Priority</th>
 					<th>Owner</th>
 					<th>Title</th>
 				</thead>
 				<tbody>
-					<td>1</td>
-					<td>Open</td>
-					<td>P1</td>
-					<td>Ravan</td>
-					<td>App crashes on open</td>
+					<BugRow id="1" status="Open" priority="P1" owner="Joey" title="Joey Tribbiani does not share food." />
+					<BugRow id="2" status="Closed" priority="P3" owner="Chandler" title="Ms. Chanandler Bong." />
 				</tbody>
 			</table>
+		);
+	}
+});
+
+var BugRow = React.createClass({
+	render: function() {
+		return (
+			<tr className="bugRow">
+				<td>{ this.props.id }</td>
+				<td>{ this.props.status }</td>
+				<td>{ this.props.priority }</td>
+				<td>{ this.props.owner }</td>
+				<td>{ this.props.title }</td>
+			</tr>
+		);
+	}
+})
+
+var BugAdd = React.createClass({
+	render: function() {
+		return (
+			<div className="bugAdd">
+				<span>This is where the add button will be</span>
+			</div>
+		);
+	}
+});
+
+var BugList = React.createClass({
+	render: function() {
+		return (
+			<div className="bugList">
+				<BugFilter />
+				<BugAdd />
+				<BugTable />
+			</div>
 		);
 	}
 });
